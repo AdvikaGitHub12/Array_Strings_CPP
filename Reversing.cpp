a80b939d6efcac5898cc77eb454c4668b33507e8
Advika Bhosle <advikabhosle@gmail.com>
2025-07-30 12:15:32 +0530

    Reversing an array

---
+++ Reversing.cpp
@@ -0,0 +1,27 @@
+// Name - Advika Bhosle
+// PRN - 24070123007
+// Batch - EnTC - A1 
+// Expt - 7
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int arr[5] = {7, 14, 21, 28, 35};
+
+    cout << "Reversed array: ";
+    for (int i = 4; i >= 0; i--) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+    
+}
+
+//output
+/*
+Reversed array: 35 28 21 14 7 
+
+=== Code Execution Successful ===
+/*
