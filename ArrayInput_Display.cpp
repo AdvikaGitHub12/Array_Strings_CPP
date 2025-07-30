7cebbb910572025e4a562f101e98fb1c1d3bce67
Advika Bhosle <advikabhosle@gmail.com>
2025-07-30 12:09:07 +0530

    Displaying Array input

---
+++ ArrayInput_Display.cpp
@@ -0,0 +1,33 @@
+// Name - Advika Bhosle
+// PRN - 24070123007
+// Batch - EnTC - A1 
+// Expt - 7
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int arr[6];
+    cout<<"enter 6 numbers:";
+    for ( int i=0; i<6; i++){
+        cin>>arr[i];
+}
+        cout<<"the given array is:";
+        for(int i=0; i<6; i++){
+             cout<<arr[i]<<endl;
+}
+        return 0;
+    }
+
+//output
+/*
+enter 6 numbers:2 4 6 3 5 7
+the given array is:2
+4
+6
+3
+5
+7
+
+=== Code Execution Successful ===
+/*
