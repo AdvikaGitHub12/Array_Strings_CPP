86499e23d31304339f020beb2ec74b9d89803330
Advika Bhosle <advikabhosle@gmail.com>
2025-07-31 09:59:11 +0530

    Basic string

---
+++ String_Array.cpp
@@ -0,0 +1,21 @@
+// Name - Advika Bhosle
+// PRN - 24070123007
+// Batch - EnTC - A1
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    char str1[4] = "C++";                    
+    char str2[] = {'C', '+', '+', '\0'};     
+    char str3[5] = {'C', '+', '+', ',', '\0'}; 
+    string str4 = "C++";                     
+    return 0;
+}
+
+//output
+/*
+
+=== Code Execution Successful ===
+*/
