81df8d546e8f0e56e451e1b8c490a4b11abd390d
Advika Bhosle <advikabhosle@gmail.com>
2025-07-31 10:05:39 +0530

    Displaying String input

---
+++ inputString.cpp
@@ -0,0 +1,26 @@
+// Name - Advika Bhosle
+// PRN - 24070123007
+// Batch - EnTC - A1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string sentence;
+
+    cout << "Enter a sentence: ";
+    getline(cin, sentence);  
+
+    cout << "You entered: " << sentence << endl;
+
+    return 0;
+}
+
+//output
+/*
+Enter a sentence: C++ is fun
+You entered: C++ is fun
+
+
+=== Code Execution Successful ===
+/*
