3132f3cd7ad002bcf0c251b582e392b11d6fe785
Advika Bhosle <advikabhosle@gmail.com>
2025-07-31 10:09:07 +0530

    concatenation of strings

---
+++ concat.cpp
@@ -0,0 +1,32 @@
+// Name - Advika Bhosle
+// PRN - 24070123007
+// Batch - EnTC - A1
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    string str1, str2, result;
+
+    cout << "Enter first word: ";
+    cin >> str1;
+
+    cout << "Enter second word: ";
+    cin >> str2;
+
+    result = str1 + " " + str2;  
+
+    cout << "Concatenated string: " << result << endl;
+
+    return 0;
+}
+
+//output
+/*
+Enter first word: Hello
+Enter second word: world
+Concatenated string: Hello world
+
+
+=== Code Execution Successful ===
+/*
